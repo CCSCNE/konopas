35c81bdc32646e6abfd07e9c26dc175be04d892c
Karl R. Wurst <karl@w-sts.com>
2014-03-15 10:58:59 -0400

    Added more program items and people

--- data/people.js
+++ data/people.js
@@ -14,4 +14,111 @@ var people = [
         "prog": [ "6"],
         "links": {},
         "bio": "University of New Haven"
-    }]
+    },
+    {
+        "id": "3",
+        "name": [ "Heidi", "Ellis"],
+        "tags": [],
+        "prog": [ "8"],
+        "links": {},
+        "bio": "Western New England University"
+    },
+    {
+        "id": "4",
+        "name": [ "Darci", "Burdge"],
+        "tags": [],
+        "prog": [ "8"],
+        "links": {},
+        "bio": "Nassau Community College"
+    },
+    {
+        "id": "5",
+        "name": [ "Lori", "Postner"],
+        "tags": [],
+        "prog": [ "8"],
+        "links": {},
+        "bio": "Nassau Community College"
+    },
+    {
+        "id": "6",
+        "name": [ "Gregory", "Hislop"],
+        "tags": [],
+        "prog": [ "8"],
+        "links": {},
+        "bio": "Drexel University"
+    },
+    {
+        "id": "7",
+        "name": [ "Franklyn", "Turbak"],
+        "tags": [],
+        "prog": [ "10"],
+        "links": {},
+        "bio": "Wellesley College"
+    },
+    {
+        "id": "8",
+        "name": [ "Shaileen", "Pokress"],
+        "tags": [],
+        "prog": [ "10"],
+        "links": {},
+        "bio": "Massachusetts Institute of Technology"
+    },
+    {
+        "id": "9",
+        "name": [ "Mark", "Sherman"],
+        "tags": [],
+        "prog": [ "10"],
+        "links": {},
+        "bio": "University of Massachusetts Lowell"
+    },
+    {
+        "id": "10",
+        "name": [ "Darren", "Lim"],
+        "tags": [],
+        "prog": [ "12"],
+        "links": {},
+        "bio": "Siena College"
+    },
+    {
+        "id": "11",
+        "name": [ "Timoth", "Lederman"],
+        "tags": [],
+        "prog": [ "12"],
+        "links": {},
+        "bio": "Siena College"
+    },
+    {
+        "id": "12",
+        "name": [ "Stoney", "Jackson"],
+        "tags": [],
+        "prog": [ "13"],
+        "links": {},
+        "bio": "Western New England University"
+    },
+    {
+        "id": "13",
+        "name": [ "Karl", "Wurst"],
+        "tags": [],
+        "prog": [ "13"],
+        "links": {},
+        "bio": "Worcester State University"
+    },
+    {
+        "id": "14",
+        "name": [ "Joanmarie", "Diggs"],
+        "tags": [],
+        "prog": [ "13"],
+        "links": {},
+        "bio": "Gnome Foundation"
+    },
+    {
+        "id": "15",
+        "name": [ "Jim", "Hendler"],
+        "tags": [],
+        "prog": [ "14"],
+        "links": {},
+        "bio": "Director, Rensselaer Institute for Data Exploration and Applications, Rensselaer Polytechnic Institute"
+    }
+
+
+]
--- data/program.js
+++ data/program.js
@@ -67,4 +67,145 @@ var program = [
             { "id": "2", "name": "Christopher Martinez" }
         ],
         "desc": ""
-    }]
+    },
+    {
+        "id": "7",
+        "title": "Workshop II Registration",
+        "tags": [ "Workshop" ],
+        "date": "2014-04-25",
+        "time": "08:00",
+        "mins": "60",
+        "loc": [ "Room" ],
+        "people": [],
+        "desc": "Continental Breakfast available"
+    },
+    {
+        "id": "8",
+        "title": "Workshop II: Getting Started in Open Source - A Tour of a Real Project",
+        "tags": [ "Workshop" ],
+        "date": "2014-04-25",
+        "time": "09:00",
+        "mins": "180",
+        "loc": [ "Room" ],
+        "people": [
+            { "id": "3", "name": "Heidi Ellis" },
+            { "id": "4", "name": "Darci Burdge" },
+            { "id": "5", "name": "Lori Postner" },
+            { "id": "6", "name": "Gregory Hislop" }
+        ],
+        "desc": ""
+    },
+    {
+        "id": "9",
+        "title": "Workshop III Registration",
+        "tags": [ "Workshop" ],
+        "date": "2014-04-25",
+        "time": "08:00",
+        "mins": "60",
+        "loc": [ "Room" ],
+        "people": [],
+        "desc": "Continental Breakfast available"
+    },
+    {
+        "id": "10",
+        "title": "Workshop III: Mobile Computational Thinking with App Inventor 2",
+        "tags": [ "Workshop" ],
+        "date": "2014-04-25",
+        "time": "09:00",
+        "mins": "180",
+        "loc": [ "Room" ],
+        "people": [
+            { "id": "7", "name": "Franklyn Turbak" },
+            { "id": "8", "name": "Shaileen Polress" },
+            { "id": "9", "name": "Mark Sherman" }
+        ],
+        "desc": ""
+    },
+    {
+        "id": "11",
+        "title": "Workshop IV Registration",
+        "tags": [ "Workshop" ],
+        "date": "2014-04-25",
+        "time": "08:00",
+        "mins": "60",
+        "loc": [ "Room" ],
+        "people": [],
+        "desc": "Continental Breakfast available"
+    },
+    {
+        "id": "12",
+        "title": "Workshop IV: Teaching Life Lessons in a Capstone Course: Software Engineering as a Finishing School",
+        "tags": [ "Workshop" ],
+        "date": "2014-04-25",
+        "time": "09:00",
+        "mins": "180",
+        "loc": [ "Room" ],
+        "people": [
+            { "id": "10", "name": "Darren Lim" },
+            { "id": "11", "name": "Timoth Lederman" }
+        ],
+        "desc": ""
+    },
+    {
+        "id": "13",
+        "title": "Hackfest",
+        "tags": [ "Hackfest" ],
+        "date": "2014-04-25",
+        "time": "14:30",
+        "mins": "570",
+        "loc": [ "Room" ],
+        "people": [
+            { "id": "3", "name": "Heidi Ellis" },
+            { "id": "12", "name": "Stoney Jackson" },
+            { "id": "13", "name": "Karl Wurst" },
+            { "id": "14", "name": "Joanmarie Diggs" }
+        ],
+        "desc": ""
+    },
+    {
+        "id": "14",
+        "title": "Keynote 1: Big Data Meets Computer Science",
+        "tags": [ "Keynote" ],
+        "date": "2014-04-25",
+        "time": "13:15",
+        "mins": "60",
+        "loc": [ "Room" ],
+        "people": [
+            { "id": "15", "name": "Jim Hendler" }
+        ],
+        "desc": ""
+    },
+    {
+        "id": "16",
+        "title": "Registration and Hospitality",
+        "tags": [ "Registration" ],
+        "date": "2014-04-25",
+        "time": "10:00",
+        "mins": "240",
+        "loc": [ "Room" ],
+        "people": [],
+        "desc": ""
+    },
+    {
+        "id": "17",
+        "title": "Welcome from Dean",
+        "tags": [ "Keynote" ],
+        "date": "2014-04-25",
+        "time": "13:00",
+        "mins": "15",
+        "loc": [ "Room" ],
+        "people": [],
+        "desc": ""
+    },
+    {
+        "id": "18",
+        "title": "Break 1",
+        "tags": [ "Break" ],
+        "date": "2014-04-25",
+        "time": "14:15",
+        "mins": "15",
+        "loc": [ "Room" ],
+        "people": [],
+        "desc": ""
+    }
+]
