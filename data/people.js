507e8583c5a05389418913ad8b761678ca31e639
Karl R. Wurst <karl@w-sts.com>
2014-03-15 09:59:09 -0400

    First pass at modifying for CCSCNE 2014

--- data/people.js
+++ data/people.js
@@ -1,1 +1,17 @@
-sample/people.js
+var people = [
+    {
+        "id": "1",
+        "name": [ "David", "Eggert"],
+        "tags": [],
+        "prog": [ "6"],
+        "links": {},
+        "bio": "University of New Haven"
+    },
+    {
+        "id": "2",
+        "name": [ "Christopher", "Martinez"],
+        "tags": [],
+        "prog": [ "6"],
+        "links": {},
+        "bio": "University of New Haven"
+    }]
--- data/program.js
+++ data/program.js
@@ -1,1 +1,70 @@
-sample/program.js
+var program = [
+    {
+        "id": "1",
+        "title": "Programming contest venues open for practice",
+        "tags": [ "Programming Contest" ],
+        "date": "2014-04-25",
+        "time": "08:00",
+        "mins": "60",
+        "loc": [ "Room" ],
+        "people": [],
+        "desc": "Continental Breakfast available"
+    },    
+    {
+        "id": "2",
+        "title": "Programming contest briefing",
+        "tags": [ "Programming Contest" ],
+        "date": "2014-04-25",
+        "time": "09:00",
+        "mins": "15",
+        "loc": [ "Room" ],
+        "people": [],
+        "desc": "Short briefing"
+    },    
+    {
+        "id": "3",
+        "title": "Programming Contest",
+        "tags": [ "Programming Contest" ],
+        "date": "2014-04-25",
+        "time": "09:15",
+        "mins": "165",
+        "loc": [ "Room" ],
+        "people": [],
+        "desc": ""
+    },
+    {
+        "id": "4",
+        "title": "Programming Contest Lunch",
+        "tags": [ "Programming Contest" ],
+        "date": "2014-04-25",
+        "time": "12:00",
+        "mins": "30",
+        "loc": [ "Room" ],
+        "people": [],
+        "desc": ""
+    },
+    {
+        "id": "5",
+        "title": "Workshop I Registration",
+        "tags": [ "Workshop" ],
+        "date": "2014-04-25",
+        "time": "08:00",
+        "mins": "60",
+        "loc": [ "Room" ],
+        "people": [],
+        "desc": "Continental Breakfast available"
+    },
+    {
+        "id": "6",
+        "title": "Workshop I: Using the NAO Humanoid Robot in the Classroom",
+        "tags": [ "Workshop" ],
+        "date": "2014-04-25",
+        "time": "09:00",
+        "mins": "180",
+        "loc": [ "Room" ],
+        "people": [
+            { "id": "1", "name": "David Eggert" },
+            { "id": "2", "name": "Christopher Martinez" }
+        ],
+        "desc": ""
+    }]
